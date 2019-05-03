ede6a2c7abecfc731f7a43a8c12932c752a0d8ca
Darryl <darrylmangibin@gmail.com>
2019-05-04 06:04:17 +0800

    fixed action types

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,15 +1,17 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
 const initialState = {
   isSignedIn: null
 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
-    case 'SIGN_IN':
+    case SIGN_IN:
       return {
         ...state,
         isSignedIn: true
       }
-    case 'SIGN_OUT':
+    case SIGN_OUT:
       return {
         ...state,
         isSignedIn: false
