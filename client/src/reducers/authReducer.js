7df33ebc0ebc111246e61c10b144cc59af98cf4e
Darryl <darrylmangibin@gmail.com>
2019-05-04 07:05:57 +0800

    handling form submission

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,8 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const initialState = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 }
 
 export default (state = initialState, action) => {
@@ -9,12 +10,14 @@ export default (state = initialState, action) => {
     case SIGN_IN:
       return {
         ...state,
-        isSignedIn: true
+        isSignedIn: true,
+        userId: action.payload
       }
     case SIGN_OUT:
       return {
         ...state,
-        isSignedIn: false
+        isSignedIn: false,
+        userId: null
       }
     default:
       return state
