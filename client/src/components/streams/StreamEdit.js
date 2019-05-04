83640c1f51d5a23494d122b8ae179d806e590a76
Darryl <darrylmangibin@gmail.com>
2019-05-04 22:56:41 +0800

    REST-Based React App

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
-const StreamEdit = () => {
-  return (
-    <div>
-      StreamEdit
-    </div>
-  );
+class StreamEdit extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues)
+  }
+
+  render() {
+    if(!this.props.stream) {
+      return (
+        <div>Loading...</div>
+      )
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')} 
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
 };
 
-export default StreamEdit;
+const mapStateToPrrops = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToPrrops, {
+  fetchStream,
+  editStream
+})(StreamEdit);
---
+++ client/src/history.js
@@ -0,0 +1,7 @@
+// import createHistory from 'history/createBrowserHistory';
+
+// export default createHistory();
+
+const createHistory = require('history');
+
+module.exports = createHistory.createBrowserHistory()
